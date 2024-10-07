ea465440e8ef63436f9b03ff93582d8ff5533ce0
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2024-10-07 19:10:42 -0300

    fix: import name enhanced

--- cypress/e2e/pet/get-pet-test.cy.ts
+++ cypress/e2e/pet/get-pet-test.cy.ts
@@ -1,6 +1,6 @@
 import commonResponses from '@fixtures/responses/commons.json';
 import petPayloads from '@fixtures/payloads/pet.json';
-import testData from '@fixtures/data/pet/data.json';
+import petTestData from '@fixtures/data/pet/data.json';
 
 // Test suite name
 describe('TS Get Pet - Find pet by ID', () => {
@@ -22,14 +22,14 @@ describe('TS Get Pet - Find pet by ID', () => {
     });
   });
   it('TC: Retrieve a pet with an invalid ID format', function () {
-    cy.getPetById(testData.post.invalidId).then((response) => {
+    cy.getPetById(petTestData.post.invalidId).then((response) => {
       expect(response.status).to.eq(
         commonResponses.httpStatus.badRequest.statusCode
       );
     });
   });
   it('TC: Retrieve a pet with a non-existent ID', function () {
-    cy.getPetById(testData.post.nonExistentId).then((response) => {
+    cy.getPetById(petTestData.post.nonExistentId).then((response) => {
       expect(response.status).to.eq(
         commonResponses.httpStatus.notFound.statusCode
       );
